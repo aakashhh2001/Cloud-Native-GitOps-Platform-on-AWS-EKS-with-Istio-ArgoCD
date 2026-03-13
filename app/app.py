7b559fa134d5ae87cea633ee27fc0c70032d6d88
aakashhh2001 <akashk082001@gmail.com>
2026-03-13 10:45:15 +0530

    Bump app to Version 2.0 to test full GitOps pipeline

--- app/app.py
+++ app/app.py
@@ -4,8 +4,8 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    message = os.environ.get('APP_MESSAGE', 'Version 1.0 - Hello from EKS + Istio!')
-    return f'<h1 style="color:blue">{message}</h1>'
+    message = os.environ.get('APP_MESSAGE', 'Version 2.0 - My CI/CD Pipeline Works!')
+    return f'<h1 style="color:green">{message}</h1>'
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
